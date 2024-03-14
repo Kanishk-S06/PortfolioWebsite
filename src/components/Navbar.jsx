9d1fbcdc275215bc892c06fa67a9f5a024029470
Kanishk-S06 <kanishksharma2609@gmail.com>
2024-03-15 00:31:40 +0530

    Navbar Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,169 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import NavLink from "./navLink";
+import { motion } from "framer-motion";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
+  return (
+    <div className="h-full flex items-center justify-between px-2 sm:px-4 md:px-8 lg:px-12 xl:px-24 text-xl">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-2 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
+      {/* LOGO */}
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center lg:ml-52">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">Kanishk</span>
+          <span className="w-16 h-8 rounded bg-white text-black flex items-center justify-center">
+            Sharma
+          </span>
+        </Link>
+      </div>
+      {/* SOCIAL */}
+      <div className="hidden md:flex gap-2 w-1/3 ml-96">
+        <Link href="https://github.com/Kanishk-S06">
+          <Image
+            src="/github.png"
+            alt=""
+            width={24}
+            height={24}
+            className="mx-1"
+          />
+        </Link>
+        <Link href="https://www.instagram.com/_kanishk_sharma06?igsh=MWRhbnlvaTI2cWhxZg==">
+          <Image
+            src="/instagram.png"
+            alt=""
+            width={24}
+            height={24}
+            className="mx-1"
+          />
+        </Link>
+        <Link href="https://www.linkedin.com/in/kanishk-sharma-b7a46424b/">
+          <Image
+            src="/linkedin.png"
+            alt=""
+            width={24}
+            height={24}
+            className="mx-1"
+          />
+        </Link>
+      </div>
+
+      {/* RESPONSIVE MENU */}
+      <div className="md:hidden">
+        {/* MENU BUTTON */}
+        <button
+          className="w-8 h-6 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <motion.div
+            variants={topVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-8 h-1 bg-black rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-8 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-8 h-1 bg-black rounded origin-left"
+          ></motion.div>
+        </button>
+        {/* MENU LIST */}
+        {open && (
+          <motion.div
+            variants={listVariants}
+            initial="closed"
+            animate="opened"
+            className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-4 text-2xl z-40"
+          >
+            {links.map((link) => (
+              <motion.div
+                variants={listItemVariants}
+                className=""
+                key={link.title}
+              >
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
