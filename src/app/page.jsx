22fc6eff65841cbc8a619f18ffc223c49356511b
Kanishk-S06 <kanishksharma2609@gmail.com>
2024-03-17 13:25:16 +0530

    resume and home page added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,20 +20,24 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Crafting Digital Experiences, Designing Tomorrow.
+            Forging Digital Paths, Building Tomorrow.
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
+            Welcome to my digital domain, where full-stack prowess and creative
+            flair collide. With expertise in front-end finesse and back-end
+            wizardry, my portfolio showcases versatile projects blending design
+            and functionality seamlessly. Let's innovate together, shaping the
+            digital landscape with every line of code.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
-              View My Work
-            </button>
+            <a href="/resume.pdf" download="resume">
+              <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+                My Resume
+              </button>
+            </a>
+
             <button className="p-4 rounded-lg ring-1 ring-black">
               Contact Me
             </button>
